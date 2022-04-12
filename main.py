055728737ab853c027bbb60e034e33eec9eb6995
Aze1cool <wot2change@gmail.com>
2022-04-12 14:47:07 +0300

    Block Letters Test Upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Azamat Khafizov
+#Python
+#Project Name - Block Letters
+print("   A      ZZZZZZZ     A     M     M     A    TTTTTTT")
+print("  A A           Z    A A    MM   MM    A A      T   ")
+print(" A   A         Z    A   A   M M M M   A   A     T   ")
+print("AAAAAAA      Z     AAAAAAA  M  M  M  AAAAAAA    T   ")
+print("A     A    Z       A     A  M     M  A     A    T   ")
+print("A     A   Z        A     A  M     M  A     A    T   ")
+print("A     A   ZZZZZZZ  A     A  M     M  A     A    T   ")

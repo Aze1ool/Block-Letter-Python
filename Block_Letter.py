41f81ee776758f85fc30e2b4b7f95fd2367c6677
Azamat Khafizov <80072898+Aze1ool@users.noreply.github.com>
2022-04-14 13:51:55 +0300

    Update Block_Letter.py

--- Block_Letter.py
+++ Block_Letter.py
@@ -1,6 +1,12 @@
 # Azamat Khafizov
 # Python
 # Project Name - Block Letters
+"""
+ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+This is simple project where it displays the initials of your my name in block letters as shown and dip your toes into ASCII art.
+
+"""
+
 print("   A      ZZZZZZZ     A     M     M     A    TTTTTTT")
 print("  A A           Z    A A    MM   MM    A A      T   ")
 print(" A   A         Z    A   A   M M M M   A   A     T   ")
